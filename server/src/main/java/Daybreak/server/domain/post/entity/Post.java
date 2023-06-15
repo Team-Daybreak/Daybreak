a896312185ee831da3dcc9f051a3a6c5b821a3aa
KunWoo Shin <cyberspecterr@gmail.com>
2023-06-16 00:55:36 +0900

    Add Relation Mapping to User

--- server/src/main/java/Daybreak/server/domain/post/entity/Post.java
+++ server/src/main/java/Daybreak/server/domain/post/entity/Post.java
@@ -1,5 +1,6 @@
 package Daybreak.server.domain.post.entity;
 
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -7,22 +8,58 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long postId;
+    private Long id;
 
-    @Column(nullable = false)
+    @Setter
+    @Column(nullable = false, length = 50)
     private String title;
 
+    @Setter
     @Column(nullable = false)
     private String content;
+
+    // --- Relation Mapping ---
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "MEMBER_ID")
+//    @JsonIgnore
+//    private User user;
+
+    @Setter
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Kind kind;
+
+    @Setter
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Status status = Status.ACTIVE;
 
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDateTime ModifiedAt;
+    private LocalDateTime modifiedAt;
+
+    private Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Post of(String title, String content) {
+        return new Post(title, content);
+    }
+
+    // --- Functions ---
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.getModifiedAt() = LocalDateTime.now();
+    }
 }
