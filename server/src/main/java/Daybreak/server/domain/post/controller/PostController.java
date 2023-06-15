234a3a4dac5860dd6342137c33fd7ba9bf7b5c3f
KunWoo Shin <cyberspecterr@gmail.com>
2023-06-16 00:57:13 +0900

    Add Post CRUD APIs

--- server/src/main/java/Daybreak/server/domain/post/controller/PostController.java
+++ server/src/main/java/Daybreak/server/domain/post/controller/PostController.java
@@ -1,4 +1,46 @@
 package Daybreak.server.domain.post.controller;
 
+import Daybreak.server.domain.post.dto.PostDto;
+import Daybreak.server.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    /**
+     * @author 신건우
+     * @param data : 글 생성 데이터
+     * @desc 게시글 생성
+     */
+    @PostMapping
+    public ResponseEntity<PostDto.Response> createPost(@RequestBody PostDto.Post data) {
+        return new ResponseEntity<PostDto.Response>(postService.create(data), HttpStatus.CREATED);
+    }
+
+    /**
+     * @author 신건우
+     * @param data : 글 수정 데이터
+     * @desc 게시글 수정
+     */
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostDto.Response> updatePost(@RequestBody PostDto.Patch data) {
+        return new ResponseEntity<PostDto.Response>(postService.update(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletePost(@PathVariable("id") Long id) {
+       return new ResponseEntity(postService. HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto.Response> getPost(@PathVariable("id") Long id) {
+        return new ResponseEntity<PostDto.Response>(postService.delete(id), HttpStatus.OK);
+    }
 }
