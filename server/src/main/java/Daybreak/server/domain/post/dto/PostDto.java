355b6b33476a09486823148cee95df2a56deea08
KunWoo Shin <cyberspecterr@gmail.com>
2023-06-16 00:57:43 +0900

    Add Request & Response Data

--- server/src/main/java/Daybreak/server/domain/post/dto/PostDto.java
+++ server/src/main/java/Daybreak/server/domain/post/dto/PostDto.java
@@ -1,4 +1,52 @@
 package Daybreak.server.domain.post.dto;
 
+import Daybreak.server.domain.post.entity.Kind;
+import Daybreak.server.domain.post.entity.Post;
+import Daybreak.server.domain.post.entity.Status;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 public class PostDto {
+
+    @Getter @Builder
+    @AllArgsConstructor
+    public static class Post {
+        private String title;
+        private String content;
+        private String kind;
+    }
+
+    @Getter @Setter @Builder
+    @AllArgsConstructor
+    public static class Patch {
+        private String title;
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Response {
+        private Long id;
+        private String title;
+        private String content;
+        private Kind kind;
+        private Status status;
+        private LocalDateTime createdAt;
+
+        public static PostDto.Response of(Long id, String title, String content, Kind kind, Status status, LocalDateTime createdAt) {
+            return new PostDto.Response(id, title, content, kind, status, createdAt);
+        }
+
+        public static PostDto.Response fromEntity(Daybreak.server.domain.post.entity.Post entity) {
+            return new PostDto.Response(
+                    entity.getId(),
+                    entity.getTitle(),
+                    entity.getContent(),
+                    entity.getKind(),
+                    entity.getStatus(),
+                    entity.getCreatedAt()
+            );
+        }
+    }
 }
