28af7248f96b568b7816ea98baa4029a32173adb
KunWoo Shin <cyberspecterr@gmail.com>
2023-06-16 00:58:30 +0900

    Add GET & DELETE Query

--- server/src/main/java/Daybreak/server/domain/post/repository/PostRepository.java
+++ server/src/main/java/Daybreak/server/domain/post/repository/PostRepository.java
@@ -1,4 +1,22 @@
 package Daybreak.server.domain.post.repository;
 
-public class PostRepository {
+import Daybreak.server.domain.post.entity.Kind;
+import Daybreak.server.domain.post.entity.Post;
+import Daybreak.server.domain.post.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query(value = "SELECT p FROM Post p WHERE p.id = :id")
+    Optional<Post> findById(@Param("id") Long id);
+
+    @Query(value = "SELECT p FROM Post p WHERE p.id = :id AND p.kind = :kind AND p.status = :status")
+    Optional<Post> findByIdAndCheckStatus(@Param("id") Long id, @Param("kind") Kind kind, @Param("status") Status status);
+
+    @Query(value = "DELETE FROM Post p WHERE p.id = :id")
+    void deleteById(@Param("id") Long id);
 }
