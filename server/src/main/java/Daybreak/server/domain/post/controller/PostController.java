62430f390dcab0fc6a27fbcc8dfd9336de698f2a
KunWoo Shin <cyberspecterr@gmail.com>
2023-06-17 23:06:16 +0900

    Develop Post CRUD

--- server/src/main/java/Daybreak/server/domain/post/controller/PostController.java
+++ server/src/main/java/Daybreak/server/domain/post/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
+
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
@@ -17,30 +19,53 @@ public class PostController {
     /**
      * @author 신건우
      * @param data : 글 생성 데이터
+     * @param uid : 유저 id
      * @desc 게시글 생성
      */
     @PostMapping
-    public ResponseEntity<PostDto.Response> createPost(@RequestBody PostDto.Post data) {
-        return new ResponseEntity<PostDto.Response>(postService.create(data), HttpStatus.CREATED);
+    public ResponseEntity<PostDto.Response> createPost(
+            @RequestBody PostDto.Post data,
+            @RequestParam @Positive Long uid) {
+        return new ResponseEntity<>(PostDto.Response.fromEntity(postService.create(data, uid)), HttpStatus.CREATED);
     }
 
     /**
      * @author 신건우
      * @param data : 글 수정 데이터
+     * @param uid : 유저 id
      * @desc 게시글 수정
      */
     @PatchMapping("/{id}")
-    public ResponseEntity<PostDto.Response> updatePost(@RequestBody PostDto.Patch data) {
-        return new ResponseEntity<PostDto.Response>(postService.update(), HttpStatus.OK);
+    public ResponseEntity<PostDto.Response> updatePost(
+            @RequestBody PostDto.Patch data,
+            @RequestParam @Positive Long postId,
+            @RequestParam @Positive Long uid) {
+        return new ResponseEntity<>(PostDto.Response.fromEntity(postService.update(data, postId, uid)), HttpStatus.OK);
     }
 
+    /**
+     * @author 신건우
+     * @param id : 글 번호
+     * @param uid : 유저 id
+     * @desc 게시글 삭제
+     */
     @DeleteMapping("/{id}")
-    public ResponseEntity deletePost(@PathVariable("id") Long id) {
-       return new ResponseEntity(postService. HttpStatus.OK);
+    public ResponseEntity deletePost(
+            @PathVariable("id") Long id,
+            @RequestParam("uid") @Positive Long uid) {
+
+        postService.delete(id, uid);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    /**
+     * @author 신건우
+     * @param id : 글 번호
+     * @desc 게시글 조회
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<PostDto.Response> getPost(@PathVariable("id") Long id) {
-        return new ResponseEntity<PostDto.Response>(postService.delete(id), HttpStatus.OK);
+    public ResponseEntity<PostDto.Response> getPost(
+            @RequestParam("id") @Positive Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- server/src/main/java/Daybreak/server/domain/post/dto/PostDto.java
+++ server/src/main/java/Daybreak/server/domain/post/dto/PostDto.java
@@ -3,25 +3,38 @@ package Daybreak.server.domain.post.dto;
 import Daybreak.server.domain.post.entity.Kind;
 import Daybreak.server.domain.post.entity.Post;
 import Daybreak.server.domain.post.entity.Status;
+import Daybreak.server.domain.user.entity.Member;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 public class PostDto {
 
-    @Getter @Builder
-    @AllArgsConstructor
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Post {
         private String title;
         private String content;
-        private String kind;
+        private Kind kind;
+
+        public static PostDto.Post of(String title, String content, Kind kind) {
+            return new PostDto.Post(title, content, kind);
+        }
+
+        public Daybreak.server.domain.post.entity.Post toEntity(Member member) {
+            return Daybreak.server.domain.post.entity.Post.createOf(title, content, kind, member);
+        }
     }
 
-    @Getter @Setter @Builder
-    @AllArgsConstructor
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Patch {
         private String title;
         private String content;
+
+        public static PostDto.Patch of(String title, String content) {
+            return new PostDto.Patch(title, content);
+        }
     }
 
     @Getter
